656d9c89e14f452f374145825027a4a92533d969
Veselin Stoyanov <veselin1996@gmail.com>
2016-10-29 21:22:52 +0300

    Implemented a function that finds the number of palindromes in a specified range.

--- Homework1/02-count-palindrome-numbers-in-range/solution.rkt
+++ Homework1/02-count-palindrome-numbers-in-range/solution.rkt
@@ -13,3 +13,24 @@
 (define (palindrome? n)
   (= n (reverse-number n))
 )
+
+(define (count-palindromes-in-range a b)
+  (define (count-palindromes-in-range-iter i count)
+    (if (< i b)
+      (if (palindrome? i)
+        (count-palindromes-in-range-iter (+ i 1) (+ count 1))
+        (count-palindromes-in-range-iter (+ i 1) count)
+      )
+      count
+    )
+  )
+
+  (cond
+    [(and (> a 0) (> b 0) (< a b)) (count-palindromes-in-range-iter a 0)]
+    [else #f]
+  )
+)
+
+; Let's find the number of palindromes between 1 and 100.
+; Expected result: 18
+; (count-palindromes-in-range 1 100)
