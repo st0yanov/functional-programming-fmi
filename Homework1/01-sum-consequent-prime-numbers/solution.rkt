336490a228c9959d13e65fcb217e26c166a4c860
Veselin Stoyanov <veselin1996@gmail.com>
2016-10-29 20:38:02 +0300

    Implemented sum function for prime numbers in a range.

--- Homework1/01-sum-consequent-prime-numbers/solution.rkt
+++ Homework1/01-sum-consequent-prime-numbers/solution.rkt
@@ -16,3 +16,20 @@
     (prime-iter 2)
   )
 )
+
+(define (sum-prime-numbers n k)
+  (define (helper i counter sum)
+    (if (< counter n)
+      (if (prime? i)
+        (helper (+ i 1) (+ counter 1) (+ sum i))
+        (helper (+ i 1) counter sum)
+      )
+      sum
+    )
+  )
+  (helper (+ k 1) 0 0)
+)
+
+; Let's find the sum of the first 5 prime numbers.
+; Expected result: 28
+; (sum-prime-numbers 5 1)
