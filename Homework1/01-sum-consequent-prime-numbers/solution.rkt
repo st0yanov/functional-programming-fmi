ff67c0bbfd17b6c6c57b016773b1c763852f68a9
Veselin Stoyanov <veselin1996@gmail.com>
2016-10-29 20:13:22 +0300

    Implemented predicate function for checking primality of a number.

---
+++ Homework1/01-sum-consequent-prime-numbers/solution.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (prime? n)
+  (define square-root-of-n (sqrt n))
+
+  (define (prime-iter divisor)
+    (cond
+      [(> divisor square-root-of-n) #t]
+      [(= (remainder n divisor) 0) #f]
+      [else (prime-iter (+ divisor 1))]
+    )
+  )
+
+  (if (or (= n 1) (< n 1))
+    #f
+    (prime-iter 2)
+  )
+)
