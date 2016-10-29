49009116443f8aedb109cc90195555f843fce91a
Veselin Stoyanov <veselin1996@gmail.com>
2016-10-29 20:57:06 +0300

    Implemented reverse number function.

---
+++ Homework1/02-count-palindrome-numbers-in-range/solution.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (reverse-number n)
+  (define (reverse-number-iter n result)
+    (if (= n 0)
+      result
+      (reverse-number-iter (quotient n 10) (+ (* result 10) (remainder n 10)))
+    )
+  )
+  (reverse-number-iter n 0)
+)
